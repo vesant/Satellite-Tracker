a70b66d586fdacbacbf59a2fbc4768442a552ac8
vesant <v.goncalves2006@gmail.com>
2025-04-28 00:25:58 +0100

    Updated main.py

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ console = Console()
 
 # === configs ===
 API_KEY = 'xxxxxx-xxxxxx-xxxxxx-xxxx' # put your api from n2yo
-DEFAULT_LAT = 38.575066 # put yours
-DEFAULT_LON = -8.778905 # put yours
+DEFAULT_LAT = xxxx # put yours
+DEFAULT_LON = -xxxx # put yours
 
 # === important functions ===
 def utc_to_local(utc_timestamp):
